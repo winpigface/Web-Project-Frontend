3646435e4191b7e587cab39a5cb656f82efdf27b
winpigface <winpigface2537@gmail.com>
2023-11-25 13:38:39 +0700

    update 1

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,104 @@
+<template>
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+    <!-- (Optional) The Header -->
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          round
+          dense
+          icon="menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Admin Dashboard
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+
+    <!-- (Optional) A Drawer; you can add one more with side="right" or change this one's side -->
+    <q-drawer
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      class="bg-grey-2"
+    >
+
+    <q-list
+     >
+      <q-item-label
+          header
+        >
+          Admin Dashboard
+      </q-item-label>
+
+
+
+      <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+    </q-list>
+
+    </q-drawer>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+const linksList = [
+  {
+    title: 'Users',
+    caption: '',
+    icon: 'school',
+    link: 'admin/users'
+
+  },
+  {
+    title: 'Washing Machine',
+    caption: '',
+    icon: 'code',
+    link: 'admin/washmachine'
+  },
+  {
+    title: 'Booking',
+    caption: '',
+    icon: 'chat',
+    link: 'admin/booking'
+  },
+  {
+    title: 'Report',
+    caption: '',
+    icon: 'record_voice_over',
+    link: 'admin/report'
+  }
+]
+export default defineComponent({
+  name: 'AdminLayout',
+  components: {
+    EssentialLink
+  },
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,19 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
+  {
+    path: '/admin',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { path: '/admin/users', component: () => import('pages/AdminPage/AdminUsers.vue') },
+      { path: '/admin/washmachine', component: () => import('pages/AdminPage/AdminWash.vue') },
+
+      { path: '/admin/booking', component: () => import('pages/AdminPage/AdminBooking.vue') },
+      { path: '/admin/report', component: () => import('pages/AdminPage/AdminReport.vue') }
+
+    ]
+  },
+
 
   // Always leave this as last one,
   // but you can also remove it
