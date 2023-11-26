ccb154ff7af98643d023230b5a7eeadcdb636111
winpigface <winpigface2537@gmail.com>
2023-11-26 22:59:13 +0700

    up axios and send profile

---
+++ src/pages/UserProfilePage.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page padding>
+    <div >
+asdsa
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useLoginUserStore } from 'src/stores/LoginUser';
+import { Notify } from 'quasar';
+export default defineComponent({
+  name: 'UserProfilePage',
+  data(){
+    return{
+      Myprofile: useLoginUserStore()
+    }
+
+  },
+  methods:{
+    updateMyData(id,data){
+      const headers = {
+        "x-access-token": this.storeLogUser.accessToken
+      }
+      this.$api
+      .put('/dashboard/'+id,data,{headers})
+      .then((res)=>{
+        Notify.create({
+              type: "positive",
+              message: "Updated successfull" ,
+            });
+      })
+      .catch((err)=>{
+        Notify.create({
+              type: "negative",
+              message: err.message,
+            });
+      })
+    }
+
+
+  },
+  async mounted(){
+
+  }
+
+})
+</script>
+<style lang="scss" scoped>
+
+</style>
